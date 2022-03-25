7bd511d6e3354f4e7e4d6264f31a13d67d3ce2ab
elrilacho <80546125+elrilacho@users.noreply.github.com>
2022-03-24 22:31:46 -0500

    tema 3 listo

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -2,139 +2,159 @@
 .curso-main-container.pb-3
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-    
-    .titulo-principal
-      .h3 Producción Audiovisual
-    
-    #t_2_1.titulo-segundo
-      .h4 2.1 Infografía Animada
-    
-    p.mb-4 Propuesta infográfica en animación que refuerce un tema y matice conceptos dados de manera creativa y con intención pedagógica.
+    .titulo-principal.color-acento-contenido
+      .titulo-principal__numero
+        span 2 
+      h1 Características de la mercancía y objetos postales
+    p.mb-5 Resulta de gran importancia dentro del proceso de empaque conocer el tipo de mercancía y su naturaleza.
+      br
+      br
+      |Según su tipo:
+    .row.mb-5
+      .col-md-6.col-lg.mb-5.mb-lg-0
+        .tarjeta-avatar.tarjeta-icon
+          img(src='@/assets/curso/62.svg' alt='AvatarTop')
+          //- .tarjeta debe ir acompañado de una de una de estas clases => 
+          //- .color-primario, .color-secundario, .color-acento-contenido, .color-acento-botones
+          //- estas clases tambien tienen un modificador --borde
+          .tarjeta.bg-purple-claro.w-100
+            .p-5
+              h4 Mercancía general
+              p La conforman productos individuales, puede ser suelta o unitarizada. Dentro de la carga suelta se encuentran los productos individuales, manipulados como unidades separadas tales como cajas, barriles, fardos, etc.; y en la carga unitarizada se encuentra la mercancía que está agrupada en unidades como paletas y contenedores. 
+      .col-md-6.col-lg.mb-5.mb-lg-0
+        .tarjeta-avatar.tarjeta-icon
+          img(src='@/assets/curso/63.svg' alt='AvatarTop')
+          //- .tarjeta debe ir acompañado de una de una de estas clases => 
+          //- .color-primario, .color-secundario, .color-acento-contenido, .color-acento-botones
+          //- estas clases tambien tienen un modificador --borde
+          .tarjeta.bg-purple-claro.w-100
+            .p-5
+              h4 Mercancía granel
+              p Para el caso de la carga a granel, como el aceite, minerales y cereales, se almacenan en tanques o silos y no requiere embalaje o unitarización. 
+    p.mb-5 Por su naturaleza se dividen en carga:
+    .row.mb-5
+      .col-lg-3
+        .tarjeta.p-4
+          .d-flex.justify-content-center
+            img(src='@/assets/curso/64.svg', alt='Texto que describa la imagen' style='width: 181px').mb-4
+          .texto-seis-alt.color-secundario
+            p Perecedera (alimentos).
+      .col-lg-3
+        .tarjeta.p-4
+          .d-flex.justify-content-center
+            img(src='@/assets/curso/65.svg', alt='Texto que describa la imagen' style='width: 181px').mb-4
+          .texto-seis-alt.color-primario
+            p Frágil (vidrio, obras de arte). 
+      .col-lg-3
+        .tarjeta.p-4
+          .d-flex.justify-content-center
+            img(src='@/assets/curso/66.svg', alt='Texto que describa la imagen' style='width: 181px').mb-4
+          .texto-seis-alt.color-acento-contenido
+            p Peligrosa (municiones, combustibles, químicos, etc.).
+      .col-lg-3
+        .tarjeta.p-4
+          .d-flex.justify-content-center
+            img(src='@/assets/curso/67.svg', alt='Texto que describa la imagen' style='width: 181px').mb-4
+          .texto-seis-alt.color-acento-botones
+            p De dimensiones y pesos especiales (plantas eléctricas). 
+    p.mb-5 Para determinar el envase o empaque adecuado de cualquier producto, es importante cuestionarse sobre el tipo de producto para definir el nivel de protección necesaria, pues no todos requieren el mismo nivel de protección frente a eventualidades. 
+      br
+      br
+      |A continuación, se presenta un ejemplo de diferentes tipos de productos y sus necesidades más importantes de protección:
+    .row.justify-content-center.mb-5
+      .col-lg-6
+        LineaTiempoD.color-acento-contenido
+          .text-small(numero="1" titulo="Frutas y hortalizas")
+            p Golpes, magulladuras, temperaturas.
+            figure.mb-5
+              img(src='@/assets/curso/68.jpg', alt='Texto que describa la imagen')
+          .text-small(numero="2" titulo="Enlatados")
+            p Humedad, golpes.
+            figure.mb-5
+              img(src='@/assets/curso/69.jpg', alt='Texto que describa la imagen')
+          .text-small(numero="3" titulo="Textiles")
+            p Moho, polillas, polvo, decoloración. 
+            figure.mb-5
+              img(src='@/assets/curso/70.jpg', alt='Texto que describa la imagen')
+          .text-small(numero="4" titulo="Productos de madera ")
+            p Rayones, humedad, decoloración, combustión. 
+            figure.mb-5
+              img(src='@/assets/curso/71.jpg', alt='Texto que describa la imagen')
+          .text-small(numero="5" titulo="Productos metálicos")
+            p Humedad, rayado, golpes, rotura.
+            figure.mb-5
+              img(src='@/assets/curso/72.jpg', alt='Texto que describa la imagen')
+          .text-small(numero="6" titulo="Papel")
+            p Humedad, luz, combustión.
+            figure.mb-5
+              img(src='@/assets/curso/73.jpg', alt='Texto que describa la imagen')
+          .text-small(numero="7" titulo="Paja o similares")
+            p Humedad, compresión, combustión.
+            figure.mb-5
+              img(src='@/assets/curso/74.jpg', alt='Texto que describa la imagen')
+          .text-small(numero="8" titulo="Joyería")
+            p Pérdida, robo, rotura.
+            figure.mb-5
+              img(src='@/assets/curso/75.jpg', alt='Texto que describa la imagen')
+    p.mb-5 También es importante tener en cuenta el nivel de protección de acuerdo con el estado físico y el tipo de producto teniendo en cuenta aspectos como la durabilidad y condiciones de conservación:
+    h3.titulo-tercero Producto perecedero 
+    p.mb-5 En esta clasificación hacen parte la mayoría de los productos alimenticios y aquellos que cuentan con fecha de vencimiento; para el manejo del inventario se debe dar salida primero a los más antiguos y para estos productos se requieren medios de preservación, como control de temperatura.
+      br
+      br
+      |Según sus condiciones de conservación se pueden encontrar los siguientes tipos:
+    .titulo-sexto.color-primario
+      h5 Tabla 1
+      span Clasificación de los productos perecederos según las condiciones de conservación
+    .tabla-a.color-acento-botones.mb-5
+      table
+        thead
+          tr.bg-blue
+            th.h4 Tipo
+            th.h4 Descripción
+            th.h4 Ejemplo
+        tbody
+          tr
+            td.fw-bold Temperatura ambiente
+            td Productos que se manejan bajo condiciones de temperatura sin ninguna alteración. 
+            td Cosméticos, aseo personal, cereales, enlatados, galletas, snacks, aceites, granos.
+          tr
+            td.fw-bold Refrigerado
+            td Productos que se deben conservar a una temperatura entre 1°C y 8° C.
+            td Pescado, carne, pollo, leche, postres, frutas, verduras, flores, plantas.
+          tr
+            td.fw-bold Congelado
+            td Productos que se deben almacenar en cámaras especiales con una temperatura menos a 18 °C.
+            td Carne, pollo, pescado, pulpas, verduras, postres, productos listos para comer.
+          tr
+            td.fw-bold Fresco
+            td Productos que requieren ubicarse en lugares frescos sin necesidad de ser congelados o refrigerados, en ocasiones se necesita conservar después de abierto en la nevera.
+            td Leche, jugos, vino, chocolates, frutas y verduras.
+    h3.titulo-tercero  Producto frágil 
+    p.mb-5 Requiere de un manejo especial durante el transporte y las operaciones de embalaje, manipulación y traslado deben realizarse con cuidado, ya que sus características así lo exigen y esto se logra al utilizar una cantidad suficiente de material que le dé a la carga una amortiguación apropiada.
+      br
+      br
+      |Los puntos críticos en el transporte y distribución física de la carga frágil son: 
+    .tarjeta.contenedor-amarillo.p-5.mb-5(style=' background-color: #fff3d8 ')
+      ol.lista-ol--cuadro.lista-ol--separador
+        li 
+          .lista-ol--cuadro__vineta
+            span a
+          | El cargue y descargue.
+        li 
+          .lista-ol--cuadro__vineta
+            span b
+          | El movimiento en el vehículo de transporte.
+        li 
+          .lista-ol--cuadro__vineta
+            span c
+          | El almacenamiento y bodegaje.
+    h3.titulo-tercero Productos peligrosos 
+    p.mb-5 Son sustancias en estado sólido, líquido o gaseoso que, por sus características, pueden ocasionar daño al ser humano, al medio ambiente o a los bienes. Por ende, se les debe dar un tratamiento especial durante su almacenamiento y transporte. Para ello, hay que establecer cuáles son los materiales peligrosos que pueden poner en riesgo la vida humana o instalaciones, conocer las regulaciones y cumplirlas.
 
-    figure.mb-5
-      .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2cnKTwQ85fg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
-    #t_2_2.titulo-segundo
-      .h4 2.2 Video Motion
 
-    p.mb-4 Animación digital que crea la ilusión de movimiento mediante imágenes, fotografías, títulos, colores y diseños.
 
-    figure.mb-5
-      .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/E7-BaX6ox9s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
-    #t_2_3.titulo-segundo
-      .h4 2.3 Motion + Presentador
-
-    p.mb-4 Es el uso locutor + acompañamiento de imágenes como fondo, el estilo tiene una mezcla entre imagen viva y recursos gráficos vectoriales  se usa cuando un material tiene información de datos numéricos o gráficos de valores. 
-
-    figure.mb-5
-      .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/Ky73iaXuuTA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-
-    #t_2_4.titulo-segundo
-      .h4 2.4 Animación 2D
-
-    p.mb-4 Es el uso locutor + acompañamiento de imágenes como fondo, el estilo tiene una mezcla entre imagen viva y recursos gráficos vectoriales  se usa cuando un material tiene información de datos numéricos o gráficos de valores. 
-
-    figure.mb-5
-      .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/3b8VctSm558" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-
-    #t_2_5.titulo-segundo
-      .h4 2.5 Video interactivo
-
-    p.mb-4 Visualización de videos con diferentes tipos de preguntas o anotaciones.
-
-    figure.mb-5
-      .video
-        iframe(src="https://vizia.co/videos/94d994c5fe93f1b08a60eb/embed" allowtransparency="true" frameborder="0" scrolling="no" width="640" height="390")
-
-    #t_2_6.titulo-segundo
-      .h4 2.6 Infografías
-
-    p.mb-4 Es una herramienta de comunicación de alta precisión, el cual tiene una narrativa gráfica con un texto corto acompañada de elementos visuales que tienen como intención de captar rápidamente la atención del lector. 
-
-    figure.mb-5
-      img(src='@/assets/curso/infografia-1.jpg', alt='Infografía Fundamentos del Servicio de Interpretación')
-      figcaption Fundamentos del Servicio de Interpretación
-
-    .tarjeta.tarjeta--morada.p-3.mb-5
-      .row.justify-content-around.align-items-center
-        .col
-          .row.justify-content-between.align-items-center
-            .col.mb-3.mb-sm-0
-              .h5.mb-0 Fundamentos del Servicio de Interpretación
-              p Formato PDF
-            .col-sm-auto
-              a.boton.boton--b(:href="obtenerLink('/downloads/fundamentos_del_servicio_de_Interpretacion.pdf')" target="_blank")
-                span Descargar
-                i.fas.fa-file-download
-    
-    figure.mb-5
-      img(src='@/assets/curso/infografia-2.jpg', alt='Infografía Plan de Negocios')
-      figcaption Plan de Negocios
-
-    .tarjeta.tarjeta--morada.p-3.mb-5
-      .row.justify-content-around.align-items-center
-        .col
-          .row.justify-content-between.align-items-center
-            .col.mb-3.mb-sm-0
-              .h5.mb-0 Plan de Negocios
-              p Formato PDF
-            .col-sm-auto
-              a.boton.boton--b(:href="obtenerLink('/downloads/plan_de_negocios.pdf')" target="_blank")
-                span Descargar
-                i.fas.fa-file-download
-    
-    figure.mb-5
-      img(src='@/assets/curso/infografia-3.jpg', alt='Infografía Cobertura poblacional')
-      figcaption Cobertura poblacional
-
-    .tarjeta.tarjeta--morada.p-3.mb-5
-      .row.justify-content-around.align-items-center
-        .col
-          .row.justify-content-between.align-items-center
-            .col.mb-3.mb-sm-0
-              .h5.mb-0 Cobertura poblacional
-              p Formato PDF
-            .col-sm-auto
-              a.boton.boton--b(:href="obtenerLink('/downloads/cobertura_poblacional.pdf')" target="_blank")
-                span Descargar
-                i.fas.fa-file-download
-    
-    figure.mb-5
-      img(src='@/assets/curso/infografia-4.jpg', alt='Infografía Historia de las microfinanzas: Mohammad Yunus')
-      figcaption Historia de las microfinanzas: Mohammad Yunus
-
-    .tarjeta.tarjeta--morada.p-3.mb-5
-      .row.justify-content-around.align-items-center
-        .col
-          .row.justify-content-between.align-items-center
-            .col.mb-3.mb-sm-0
-              .h5.mb-0 Historia de las microfinanzas: Mohammad Yunus
-              p Formato PDF
-            .col-sm-auto
-              a.boton.boton--b(:href="obtenerLink('/downloads/historia_de_las_microfinanzas_mohammad_yunus.pdf')" target="_blank")
-                span Descargar
-                i.fas.fa-file-download
-    
-    figure.mb-5
-      img(src='@/assets/curso/infografia-5.jpg', alt='Infografía Psicología de la moda: sexualidad, ropa mágica, ropa malévola y ropa neurótica')
-      figcaption Psicología de la moda: sexualidad, ropa mágica, ropa malévola y ropa neurótica
-
-    .tarjeta.tarjeta--morada.p-3.mb-5
-      .row.justify-content-around.align-items-center
-        .col
-          .row.justify-content-between.align-items-center
-            .col.mb-3.mb-sm-0
-              .h5.mb-0 Psicología de la moda: sexualidad, ropa mágica, ropa malévola y ropa neurótica
-              p Formato PDF
-            .col-sm-auto
-              a.boton.boton--b(:href="obtenerLink('/downloads/psicologia_de_la_moda.pdf')" target="_blank")
-                span Descargar
-                i.fas.fa-file-download
 
 </template>
 
@@ -155,4 +175,17 @@ export default {
 }
 </script>
 
-<style lang="sass"></style>
+<style lang="sass">
+.bg-purple-claro
+  background-color: #dce0f9
+.tarjeta-avatar.tarjeta-icon
+  img
+    left: 20%
+.bg-blue
+  background-color: $color-secundario
+  th
+    color: $white
+.tabla-a
+  thead
+    border-top: 0
+</style>
--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -3,23 +3,201 @@
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
     
-    #t_3_1.pb-5.h2 tema 3.1
-    .pb-5(v-for="item of 15")
+    .titulo-principal.color-acento-contenido
+      .titulo-principal__numero
+        span 3
+      h1 Materiales y accesorios
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-4: img(src='@/assets/curso/76.png')
+      .col-lg-7: p La selección de material con el que se elaboran los empaques y embalajes es un punto crítico a la hora de evaluar la protección y conservación de los productos, pues la correcta selección del empaque ayuda a que el cliente mantenga predilección por el producto y finalmente lograr su satisfacción.
+        br
+        br
+        |Constantemente se evidencia la evolución en los empaques debido al desarrollo de nuevas tecnologías, por ejemplo, surgen nuevos materiales o sistemas de impresión. 
+    p.mb-5 Algunos aspectos importantes al seleccionar un material de empaque o embalaje son:
+    .contenedor-slyder.p-5.bg-amarillo-claro.position-relative.mb-5
+      .img-float: img(src='@/assets/curso/float1.png', alt='Texto que describa la imagen')
+      SlyderA(tipo='a')
+        .row.justify-content-center
+          .col-md-5
+            .rounded-3.overflow-hidden
+              figure
+                img(src='@/assets/curso/77.png', alt='Texto que describa la imagen')
+          .col-md-5
+            h4 Compatibilidad con el producto: 
+            p el material no debe modificar las características del producto ni hacer variar sus propiedades. Por ejemplo: los alimentos no deben tomar olor o sabor del material de empaque.
+        .row.justify-content-center
+          .col-md-5
+            .rounded-3.overflow-hidden
+              figure
+                img(src='@/assets/curso/78.png', alt='Texto que describa la imagen')
+          .col-md-5
+            h4 Resistencia mecánica:
+            p el material debe ser resistente a la compresión, desgarre, fricción o impacto. Ejemplo: las artesanías o artículos electrónicos requieren materiales resistentes a la compresión o impacto debido a su fragilidad.
+        .row.justify-content-center
+          .col-md-5
+            .rounded-3.overflow-hidden
+              figure
+                img(src='@/assets/curso/79.png', alt='Texto que describa la imagen')
+          .col-md-5
+            h4 Propiedades de protección:
+            p Dependiendo del producto se requiere impermeabilidad al agua, gases, humedad o rayos ultravioletas. Ejemplo: perfumes, medicamentos o alimentos que requieren materiales con características de impermeabilidad.
+        .row.justify-content-center
+          .col-md-5
+            .rounded-3.overflow-hidden
+              figure
+                img(src='@/assets/curso/80.png', alt='Texto que describa la imagen')
+          .col-md-5
+            h4 Propiedades de estabilidad:
+            p Es la aptitud del empaque para no modificarse debido al contacto con el producto o agentes externos. Ejemplo: el polipropileno se hace frágil cuando se somete a bajas temperaturas. 
+        .row.justify-content-center
+          .col-md-5
+            .rounded-3.overflow-hidden
+              figure
+                img(src='@/assets/curso/81.png', alt='Texto que describa la imagen')
+          .col-md-5
+            h4 Operatividad:
+            p Es la aptitud del material para ser operado dentro de una línea de empacado. Ejemplo: si un material no se puede deslizar bien, se puede atascar en la línea de empaque.
+        .row.justify-content-center
+          .col-md-5
+            .rounded-3.overflow-hidden
+              figure
+                img(src='@/assets/curso/82.png', alt='Texto que describa la imagen')
+          .col-md-5
+            h4 Conveniencia:
+            p Aspectos como el peso, ergonomía o durabilidad deben ser considerados al seleccionar el material. Ejemplo: los productos que no se consumen de inmediato y que requieren ser empacados más tiempo, requieren un material duradero. 	
+        .row.justify-content-center
+          .col-md-5
+            .rounded-3.overflow-hidden
+              figure
+                img(src='@/assets/curso/83.png', alt='Texto que describa la imagen')
+          .col-md-5
+            h4 Aspectos legales:
+            p Legislación y normativa vigente debe tenerse muy presente. Ejemplo el PVC no es permitido en algunos países, como Alemania.
+    p.mb-5 A continuación, se presentan algunos materiales a considerar para un empaque o embalaje apropiado, mencionando sus ventajas y desventajas, con el fin de tener en cuenta los usos y bondades que ofrecen.
+    .titulo-sexto.color-primario
+      h5 Tabla 2 
+      span Ventajas y desventajas de los materiales para el embalaje.
+    .tabla-a.color-acento-botones.mb-5
+      table
+        caption Nota. Tomada de Mendoza (2018)
+        thead
+          tr.bg-blue
+            th.h4 Material
+            th.h4 Ejemplo de empaque y embalaje
+            th.h4 Ventajas
+            th.h4 Desventajas
+        tbody
+          tr
+            td.fw-bold Metal
+            td Contenedores, bidones, cajas metálicas.
+            td Fácil estibado, reutilizables.
+            td Alto costo, corrosión, pesado, voluminoso.
+          tr
+            td.fw-bold Madera
+            td Cajas, canastas, estibas.
+            td Fácil de manipular.
+            td Alto costo, sensibilidad al sol y humedad, contaminable, sensible a plagas, inflamable.
+          tr
+            td.fw-bold Cartón
+            td Cajas de cartón.
+            td Económico, reciclable, fácil de manipular.
+            td Frágil, sensible a la humedad y calor, poco sólido.
+          tr
+            td.fw-bold Plástico
+            td Bolsas, cajas, contenedores.
+            td Impermeabilidad, diversidad, reutilizable.
+              br
+              |Conservan el olor y el sabor.
+              br
+              |Resistente al rasgado y envasado.
+            td Inflamable, cotoso, difícil eliminación.
+          tr
+            td.fw-bold Papel
+            td Bolsas, papel para envolver, papel parafinado.
+            td Bajo costo, fácil eliminación, reciclable.
+            td Frágil, sensible a la humedad y el calor.
+          tr
+            td.fw-bold Vidrio
+            td Botellas, frascos, botellones.
+            td Visibilidad del contenido, fácil estibado, reciclable.
+            td Frágil a golpes, pesado, voluminoso.
+    p.mb-5 Adicionalmente los empaques, envases o embalajes se pueden clasificar de acuerdo con:
+    h3.titulo-tercero La capacidad de resistencia
+    .row.mb-5
+      .col-lg-4.mb-3
+        .tarjeta.rounded-0.color-alt3.h-100.p-4
+          .icon-hover.d-flex.justify-content-center.mb-3.px-5
+            img(src='@/assets/curso/84.svg', alt='Texto que describa la imagen')
+          .text-center
+            h4 Autoportante
+            p El envase primario soporta por sí mismo el peso de otros productos sin que se deformen o dañen. <b>Por ejemplo, envases de vidrio o metal.</b>
+      .col-lg-4.mb-3
+        .tarjeta.rounded-0.color-alt3.h-100.p-4
+          .icon-hover.d-flex.justify-content-center.mb-3.px-5
+            img(src='@/assets/curso/85.svg', alt='Texto que describa la imagen')
+          .text-center
+            h4 Semiportante
+            p El envase primario no es lo suficiente rígido para soportar por sí solo el esfuerzo de compresión que se genera durante su almacenamiento o transporte. <b>Por ejemplo, botellas de plástico, cajas de cereales.</b>
+      .col-lg-4.mb-3
+        .tarjeta.rounded-0.color-alt3.h-100.p-4
+          .icon-hover.d-flex.justify-content-center.mb-3.px-5
+            img(src='@/assets/curso/86.svg', alt='Texto que describa la imagen')
+          .text-center
+            h4 No portante
+            p El envase primario no aporta ninguna resistencia al producto interno, <b>por ejemplo, galletas, frutas y verduras, bolsas de snacks.</b>
+    h3.titulo-tercero El uso 
+    .row
+      .col-lg-6
+        .tarjeta-avatar-b.mb-5
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/87.svg' alt='AvatarTop')
+            img(src='@/assets/curso/88.svg' alt='AvatarTop')
+          .tarjeta.bg-purple-claro
+            .p-4
+              h4 Retornable
+              p Corresponde a envases que cuentan con características de resistencia para ser llenados y utilizados varias veces y para los cuales se debe tener una logística de retorno, por ejemplo, los envases de vidrio de gaseosas o cervezas.
+      .col-lg-6
+        .tarjeta-avatar-b.mb-5
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/87.svg' alt='AvatarTop')
+            img(src='@/assets/curso/88.svg' alt='AvatarTop')
+          .tarjeta.bg-purple-claro
+            .p-4
+              h4 No retornable
+              p Son aquellos que, de acuerdo con su resistencia, solo pueden ser llenados y utilizados una vez, por ejemplo, latas de cerveza, atunes, bolsas de galletas, botellas plásticas, etc. 
+    h3.titulo-tercero El tipo de material y la presentación
+    .titulo-sexto.color-primario
+      h5 Tabla 3
+      span Clasificación según el tipo de material y la presentación.
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-10
+        .tabla-a.color-acento-botones.mb-5
+          table
+            caption Leyenda de la tabla
+            thead
+              tr.bg-blue
+                th.h4 Tipo
+                th.h4 Material
+                th.h4 Presentación
+            tbody
+              tr
+                td.fw-bold Envase
+                td Metal, papel, plástico, vidrio, madera.
+                td Botellas, bolsas de papel, bolsas plásticas, barriles, bidones, baldes, latas, tapas, rollos.
+              tr
+                td.fw-bold Empaque
+                td Cartulina, cartón corrugado, plástico.
+                td Bolsas termo encogibles, plegadizas, displays.
+              tr
+                td.fw-bold Embalaje
+                td Cartón corrugado, plástico, madera.
+                td Cajas corrugadas, cajones, canastillas plásticas carretes, sacos.
+              tr
+                td.fw-bold Unidad de carga
+                td Cartón corrugado, plástico, madera.
+                td Estibas, esquineros, divisiones, stretch, zuncho.
 
-    #t_3_2.pb-5.h2 tema 3.2
-    .pb-5(v-for="item of 15")
 
-    #t_3_3.pb-5.h2 tema 3.3
-    .pb-5(v-for="item of 15")
-
-    #t_3_4.pb-5.h2 tema 3.4
-    .pb-5(v-for="item of 15")
-
-    #t_3_5.pb-5.h2 tema 3.5
-    .pb-5(v-for="item of 15")
-
-    #t_3_6.pb-5.h2 tema 3.6
-    .pb-5(v-for="item of 15")
 
 </template>
 
@@ -40,4 +218,21 @@ export default {
 }
 </script>
 
-<style lang="sass"></style>
+<style lang="sass">
+.bg-amarillo-claro
+  background-color: #fff7e6
+.bg-purple-claro
+  background-color: #dce0f9
+.tarjeta-avatar-b
+  .tarjeta-avatar-b__img
+    img:first-child
+      display: block
+    img:last-child
+      display: none
+  &:hover
+    .tarjeta-avatar-b__img
+      img:first-child
+        display: none
+      img:last-child
+        display: block
+</style>
